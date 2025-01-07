edca47e48d7408694f0cf263d9f8d585b2e2087c
Humberto Aleman Odreman <haleman.23@est.ucab.edu.ve>
2025-01-07 16:09:34 -0400

    Websocket created on website open, added function for joining game

--- client/scripts/join_script.js
+++ client/scripts/join_script.js
@@ -1,28 +1,47 @@
+const $playerIdInput = document.getElementById("player-id");
+const $gameIdInput = document.getElementById("room-code");
+
+const websocket = new WebSocket("ws://192.168.4.237:8000");
+
+websocket.addEventListener("open", () => {
+	console.log("Connected to the game servers :)");
+});
+
+websocket.addEventListener("message", (event) => {
+	let ev;
+	try {
+		ev = JSON.parse(event.data);
+	} catch (e) {
+		console.error(e);
+		return;
+	}
+
+	if (ev.type === "error") console.error(ev.text);
+	if (ev.type === "instruction")
+		window.location.href = `./fleet.html?playerId=${ev.playerId}`;
+});
+
 const createGame = () => {
-	const websocket = new WebSocket("ws://192.168.4.237:8000");
-	websocket.addEventListener("open", () => {
-		const playerId = "humberto"; // TODO: get this from html
-		console.log(`Creating game for player $${playerId}`);
-		websocket.send(
-			JSON.stringify({
-				type: "message",
-				instruction: "createGame",
-				playerId: playerId,
-			}),
-		);
-	});
+	const playerId = $playerIdInput.value;
+	console.log(`Creating game for player $${playerId}`);
+	websocket.send(
+		JSON.stringify({
+			type: "message",
+			instruction: "createGame",
+			playerId: playerId,
+		}),
+	);
+};
 
-	websocket.addEventListener("message", (event) => {
-		let ev;
-		try {
-			ev = JSON.parse(event.data);
-		} catch (e) {
-			console.error(e);
-			return;
-		}
-
-		if (ev.type === "error") console.error(ev.text);
-		if (ev.type === "instruction")
-			window.location.href = `./fleet.html?playerId=${ev.playerId}`;
-	});
+const joinGame = () => {
+	const playerId = $playerIdInput.value;
+	const gameId = $gameIdInput.value;
+	websocket.send(
+		JSON.stringify({
+			type: "message",
+			instruction: "joinGame",
+			gameId: gameId,
+			playerId: playerId,
+		}),
+	);
 };
