5296896cbea740ef645a17bb258dbda959909b45
Humberto Aleman Odreman <haleman.23@est.ucab.edu.ve>
2025-01-07 22:47:07 -0400

    Added gameId to code

--- client/scripts/boat_placement.js
+++ client/scripts/boat_placement.js
@@ -113,6 +113,8 @@ const url = new URL(url_string);
 const gameId = url.searchParams.get("gameId");
 const playerId = url.searchParams.get("playerId");
 
+document.getElementById(gameId).innerText = gameId;
+
 const websocket = new WebSocket("ws://192.168.4.237:8000");
 
 websocket.addEventListener("open", () => {
